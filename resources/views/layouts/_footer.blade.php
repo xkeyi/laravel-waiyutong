f28c0368f1fa61f7395a404636562d0d74036024
vagrant <vagrant@homestead>
2019-08-08 07:16:42 +0000

    页面基础布局和用户认证

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      <a href="" target="_blank">听说科技</a><span style="color: #e27575;font-size: 14px;"> ❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+  </div>
+</footer>
